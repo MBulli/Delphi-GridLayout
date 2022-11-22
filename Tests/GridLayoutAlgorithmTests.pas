b50786fb0f85ff3066b14787da486db3b2964989
MBulli <mbulli135@gmail.com>
2022-11-22 22:18:13 +0100

    Added unit test project

---
+++ Tests/GridLayoutAlgorithmTests.pas
@@ -0,0 +1,65 @@
+﻿UNIT GridLayoutAlgorithmTests;
+
+INTERFACE
+
+USES
+  System.Types,
+
+  DUnitX.TestFramework,
+  GridLayout;
+
+TYPE
+  [TestFixture]
+  TGridLayoutAlgorithmTests = CLASS
+  PUBLIC
+
+    [Test]
+    PROCEDURE TestNoColumnNoRow;
+
+    [Test]
+    PROCEDURE TestNoColumnsOneRow;
+
+  END;
+
+IMPLEMENTATION
+
+{ TGridLayoutAlgorithmTests }
+
+PROCEDURE TGridLayoutAlgorithmTests.TestNoColumnNoRow;
+BEGIN
+  VAR LayoutBounds := Rect(0,0,100,100);
+
+  VAR GL := TGridLayout.Create(NIL);
+  GL.ColumnDefinition.Clear;
+  GL.RowDefinitions.Clear;
+
+  GL.Algorithm.Calculate(LayoutBounds);
+
+  VAR R := GL.Algorithm.ControlRect(0,0,Rect(42,1337,512,1024));
+
+  Assert.AreEqual(LayoutBounds, R);
+END;
+
+
+PROCEDURE TGridLayoutAlgorithmTests.TestNoColumnsOneRow;
+BEGIN
+  VAR LayoutBounds := Rect(0,0,100,100);
+
+  VAR GL := TGridLayout.Create(NIL);
+  GL.ColumnDefinition.Clear;
+  GL.RowDefinitions.Clear;
+
+  GL.AddRow(gsmPixels, 25);
+  GL.Algorithm.Calculate(LayoutBounds);
+
+  VAR R0 := GL.Algorithm.ControlRect(0,0,Rect(42,1337,512,1024));
+  VAR R1 := GL.Algorithm.ControlRect(1,0,Rect(42,1337,512,1024));
+
+  Assert.AreEqual(Rect(0,0,100,25), R0);
+  Assert.AreEqual(Rect(0,0,100,25), R1);
+END;
+
+INITIALIZATION
+  TDUnitX.RegisterTestFixture(TGridLayoutAlgorithmTests);
+
+END.
